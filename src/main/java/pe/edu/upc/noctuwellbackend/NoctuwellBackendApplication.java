61a6b3060f3dff1797b6e2b17ee06e25feed08c0
Emma <138627227+emmatatte@users.noreply.github.com>
2025-09-16 18:52:23 -0500

    Tabla Authority - Registrar + Listar v1.0

---
+++ src/main/java/pe/edu/upc/noctuwellbackend/NoctuwellBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.noctuwellbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoctuwellBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoctuwellBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/noctuwellbackend/NoctuwellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.noctuwellbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoctuwellBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
